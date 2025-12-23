ef4f5b467dbcaa896882ab1fce7c75d266f1721d
Andrew M <antique27@live.com>
2025-12-23 23:07:35 +0200

    design changes, styles, layout.

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+const presets = document.querySelectorAll('.preset');
+const amountInput = document.getElementById('amount');
+const currencySelect = document.getElementById('currency');
 const checkbox = document.getElementById('agree');
 const donateButton = document.getElementById('donateBtn');
 
+// preset buttons logic
+presets.forEach(btn => {
+    btn.addEventListener('click', () => {
+        presets.forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+
+        amountInput.value = btn.dataset.amount;
+    });
+});
+
+// ensure minimum amount = 1
+amountInput.addEventListener('input', () => {
+    if (amountInput.value < 1) {
+        amountInput.value = 1;
+    }
+});
+
+// policy checkbox
 checkbox.addEventListener('change', () => {
     donateButton.disabled = !checkbox.checked;
 });
 
+// placeholder for backend
 donateButton.addEventListener('click', () => {
-    alert("Backend not connected yet. Next step.");
+    alert(
+        `Donate ${amountInput.value} ${currencySelect.value}\n\nBackend (Stripe) is next step.`
+    );
 });
