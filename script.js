56722104e30ad8c31121837388c057f4dda1e37c
Andrew M <antique27@live.com>
2025-12-23 20:37:31 +0200

    polciy texts added

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.getElementById("donateBtn").addEventListener("click", () => {
+const checkbox = document.getElementById('agree');
+const donateButton = document.getElementById('donateBtn');
+
+checkbox.addEventListener('change', () => {
+    donateButton.disabled = !checkbox.checked;
+});
+
+donateButton.addEventListener('click', () => {
     alert("Backend not connected yet. Next step.");
 });
