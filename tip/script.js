d57fffe9673087b901d705a16d806dfaf0e68855
andrew_myroshnychenko <andrewm.pro@hotmail.com>
2026-07-27 15:45:10 +0100

    removed btn for EN version

--- tip/script.js
+++ tip/script.js
@@ -104,7 +104,7 @@ function updateCurrencySymbols() {
 currencySelect.addEventListener('change', updateCurrencySymbols);
 
 function updateDaBtn(lang) {
-    daBtn.style.display = lang === 'ua' ? 'none' : '';
+    daBtn.style.display = lang === 'en' ? 'none' : '';
 }
 
 document.addEventListener('langchange', (e) => {
